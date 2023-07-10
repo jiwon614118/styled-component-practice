72c0297abf7b6c41c6c933e2975663a2a6b2a87f
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-07-10 17:14:40 +0900

    다크모드 적용:

--- src/Darkmode.js
+++ src/Darkmode.js
@@ -1,39 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './Button';
+import styled, { ThemeProvider } from 'styled-components';
+import { dark, light } from './Theme'; // 환경별 테마 정보 가져오기
 
 function Darkmode() {
+  const [themeMode, setThemeMode] = useState('light'); // 테마 모드 세팅
+  const theme = themeMode === 'light' ? light : dark; // 테마 환경에 맞는 테마 컬러 가져오기.
+
+  const toggleTheme = () =>
+    setThemeMode(themeMode === 'light' ? 'dark' : 'light'); // 테마 변경하기 이벤트
+
   return (
-    <div className="login-img">
-      <img className="phone-img" alt="iPhone" src="/images/img1.png" />
-      <div className="loginBorder">
-        <header className="loginLogo">
-          <p>Westagram</p>
-        </header>
-        <section className="loginBox">
-          <div className="loginId">
-            <input
-              className="loginField"
-              type="text"
-              placeholder="전화번호, 사용자 이름 또는 이메일"
-            />
-          </div>
-          <div className="loginPassword">
-            <input
-              className="passwordField"
-              type="password"
-              placeholder="비밀번호"
-            />
-          </div>
-          <div className="btnField">
-            <button class="loginBtn">로그인</button>
-          </div>
-        </section>
-        <section className="passwordFind">
-          <a href="1">
-            <p>비밀번호를 잊으셨나요 ?</p>
-          </a>
-        </section>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <S.Main>
+        <Button
+          title={
+            theme === 'light'
+              ? '일반모드로 테마 변경하기'
+              : '다크모드로 테마 변경하기'
+          }
+          click={toggleTheme}
+        />
+      </S.Main>
+    </ThemeProvider>
   );
 }
+
 export default Darkmode;
+
+const S = {};
+S.Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.bgColor};
+`;
